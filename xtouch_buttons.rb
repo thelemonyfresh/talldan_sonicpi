7de49f5931bc5050a917534a138d3de1628441eb
Daniel Porter <daniel.porter@scienceexchange.com>
2018-08-03 09:25:16 -0700

    split up xtouch config into knobs and buttons

---
+++ xtouch_buttons.rb
@@ -0,0 +1,20 @@
+live_loop :xtouch_buttons do
+  use_real_time
+  midi_num, val = sync "/midi/x-touch_mini/#{xtouch_channel}/11/note_on"
+
+  if range(24,32,1).include?(midi_num) # knob_press
+    knob_num = midi_num - 13
+    new_val = 1
+  elsif range(32,39,1).include?(midi_num)
+    knob_num = midi_num - 21
+    new_val = 0.5
+  elsif range(40,47,1).include?(midi_num)
+    knob_num = midi_num - 29
+    new_val = 0
+  end
+
+  knob_state = "knob_#{knob_num}_state".to_sym
+
+
+  set knob_state, new_val
+end
--- xtouch_knobs.rb
+++ xtouch_knobs.rb
@@ -2,42 +2,15 @@
 # XTOUCH KNOBS
 #
 
+xtouch_channel = 1
+
+# set initial values for knob_x_state to zero if nil
+
 live_loop :xtouch_knobs do
   use_real_time
-  midi_num, val = sync "/midi/x-touch_mini/1/11/control_change"
+  midi_num, val = sync "/midi/x-touch_mini/#{xtouch_channel}/11/control_change"
 
   knob_state = "knob_#{midi_num}_state".to_sym
 
   set knob_state, (val/127.0)
-
-  if midi_num == 1 # start
-    set knob_state, (val/127.0)
-    control :knob1amp, (val/127.0)
-  elsif midi_num == 2 # finish
-    set knob_state, (val/127.0)
-  elsif midi_num == 3 # tuning
-    set knob_state, (val/127.0)
-  elsif midi_num == 4 # cutoff
-    set knob_state, (val/127.0)
-  elsif midi_num == 5 # attack
-    set knob_state, (val/127.0)
-  elsif midi_num == 6 # decay
-    set knob_state, (val/127.0)
-  elsif midi_num == 7 # sustain
-    set knob_state, (val/127.0)
-  elsif midi_num == 8 # release, should be -1 by default to stretch
-    set knob_state, (val/127.0)
-  elsif midi_num == 9
-    set "slider_state".to_sym, (val/127.0)
-    set_volume! (val/127.0)*1.3
-  end
-end
-
-live_loop :xtouch_buttons do
-  use_real_time
-  midi_num, val = sync "/midi/x-touch_mini/1/11/note_on"
-
-  if midi_num = 0
-    set :knob_1_state, 1
-  end
 end
