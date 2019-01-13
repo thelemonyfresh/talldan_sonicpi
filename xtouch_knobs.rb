cdcca8ad3d8cb6198e425814abd5e2f3a9463158
Daniel Porter <daniel.porter@scienceexchange.com>
2019-01-13 12:30:51 +0100

    initialize nil knob states

--- xtouch_knobs.rb
+++ xtouch_knobs.rb
@@ -4,8 +4,13 @@
 
 xtouch_channel = 2
 
-# set initial values for knob_x_state to zero if nil
+# Initialize ':knob_X_state' variables
+24.times do |n|
+  knob_state = "knob_#{n}_state".to_sym
+  set(knob_state, 0) if get(knob_state).nil?
+end
 
+# Set up knob listeners
 live_loop :xtouch_knobs do
   use_real_time
   midi_num, val = sync "/midi/x-touch_mini/#{xtouch_channel}/11/control_change"
